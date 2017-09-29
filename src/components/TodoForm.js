1e01d189fbe73463596abb8177365fa42a3ddf5e
Jiwon Jung <jw.jeoung@albamapp.com>
2017-09-29 17:08:29 +0900

    최상위에서 prop으로 내려주는 구조

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
-export default (props) => (
-  <form>
-    <input type="text"/>
-  </form>
-)
+export default (props) => {
+  const { currentTodo, changeCurrent } = props
+  const hangleInputChange = (evt) => {
+    const val = evt.target.value
+    changeCurrent(val)
+  }
+  return (
+    <form>
+      <input
+        type="text"
+        onChange={hangleInputChange}
+        value={currentTodo}
+      />
+    </form>
+  )
+}
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,15 +1,17 @@
 const initState = {
   todos: [
-    {id: 1, name:'Create Static UI', isComplete: true},
-    {id: 2, name:'Create Initial State', isComplete: false},
-    {id: 3, name:'Use state to render UI', isComplete: false}
-  ]
+    {id: 1, name: 'Create Static UI', isComplete: true},
+    {id: 2, name: 'Create Initial State', isComplete: false},
+    {id: 3, name: 'Use state to render UI', isComplete: false}
+  ],
+  currentTodo: ''
 }
 export default (state = initState, action) => {
   switch (action.type) {
     case 'TODO_ADD':
       return {...state, todos: [...state.todos, action.payload]}
-      
+    case 'CURRENT_UPDATE':
+      return {...state, currentTodo: action.payload}
     default:
       return state
   }
