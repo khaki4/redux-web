d9b9db326fa9abd6b16b2a38da203c1d93cdd0f4
정지원 <4dakto@gmail.com>
2017-09-29 21:16:22 +0900

    use redux thunk

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,22 +1,28 @@
+import { getTodos } from '../lib/todoService';
+
 const initState = {
-  todos: [
-    {id: 1, name: 'Create Static UI', isComplete: true},
-    {id: 2, name: 'Create Initial State', isComplete: false},
-    {id: 3, name: 'Use state to render UI', isComplete: false}
-  ],
+  todos: [],
   currentTodo: ''
 }
 
 const TODO_ADD = 'TODO_ADD'
+const TODOS_LOAD = 'TODOS_LOAD'
 const CURRENT_UPDATE = 'CURRENT_UPDATE'
 
-
 export const updateCurrent = (val) => ({type: CURRENT_UPDATE, payload: val})
+export const loadTodos = (todos) => ({type: TODOS_LOAD, payload: todos})
+export const fetchTodos = () => (dispatch) => {
+  getTodos()
+    .then(todos => dispatch(loadTodos(todos)))
+}
+
 
 export default (state = initState, action) => {
   switch (action.type) {
     case TODO_ADD:
       return {...state, todos: [...state.todos, action.payload]}
+    case TODOS_LOAD:
+      return {...state, todos: action.payload}
     case CURRENT_UPDATE:
       return {...state, currentTodo: action.payload}
     default:
