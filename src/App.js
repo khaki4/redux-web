eaf0a06a9b962c5836cef6fb2c98e85a8cd7aea6
Jiwon Jung <jw.jeoung@albamapp.com>
2017-09-29 18:11:39 +0900

    mapDispatchToProps 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { updateCurrent } from './reducers/todo';
 import logo from './logo.svg';
 import './App.css';
 import TodoForm from './components/TodoForm'
@@ -16,7 +18,7 @@ class App extends Component {
         <div className="Todo-App">
           <TodoForm
             currentTodo={this.props.currentTodo}
-            changeCurrent={this.props.changeCurrent}
+            changeCurrent={this.props.updateCurrent}
           />
           <TodoList todos={this.props.todos} />
         </div>
@@ -25,10 +27,13 @@ class App extends Component {
   }
 }
 
-
+// const actions = bindActionCreators({ updateCurrent }, store.dispatch);
 // store의 state를 Component의 props에 매핑해줍니다.
 const mapStateToProps = (state) => state
 
+// 컴포넌트의 특정 함수형 props 를 실행 했을 때 개발자가 지정한 action을 dispatch 하도록 설정합니다
+const mapDispatchToProps = (dispatch) => bindActionCreators({ updateCurrent }, dispatch)
+
 // connect는 React Component 를 redux store에 연결 시켜줍니다.
-const Connected = connect(mapStateToProps)(App)
+const Connected = connect(mapStateToProps, mapDispatchToProps)(App)
 export default Connected
--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { bindActionCreators } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import store from './store';
-import { updateCurrent } from './reducers/todo';
 
-// const todoChangeHandler = (val) =>
-//   store.dispatch(updateCurrent(val))
-
-// const actions = bindActionCreators({
-//   todoChangeHandler: updateCurrent
-// }, store.dispatch)
-
-const actions = bindActionCreators({ updateCurrent }, store.dispatch)
 
 ReactDOM.render(
   <Provider store={store}>
-    <App
-      changeCurrent={actions.updateCurrent}
-    />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
-
