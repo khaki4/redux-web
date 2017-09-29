0d70e029b5f7d26dbb767a75ab6bd5cb1973e714
Jiwon Jung <jw.jeoung@albamapp.com>
2017-09-29 17:26:52 +0900

    bindActionCreators 로 액션 dispatch 하기

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { bindActionCreators } from 'redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import store from './store';
 import { updateCurrent } from './reducers/todo';
 
-const todoChangeHandler = (val) =>
-  store.dispatch(updateCurrent(val))
+// const todoChangeHandler = (val) =>
+//   store.dispatch(updateCurrent(val))
+
+// const actions = bindActionCreators({
+//   todoChangeHandler: updateCurrent
+// }, store.dispatch)
+
+const actions = bindActionCreators({ updateCurrent }, store.dispatch)
 
 const render = () => {
   const state = store.getState();
@@ -15,7 +22,7 @@ const render = () => {
     <App
       todos={state.todos}
       currentTodo={state.currentTodo}
-      changeCurrent={todoChangeHandler}
+      changeCurrent={actions.updateCurrent}
     />,
     document.getElementById('root')
   );
