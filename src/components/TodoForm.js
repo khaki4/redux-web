c891bf2dc921c9601809d6657962cf61312c4fb6
정지원 <4dakto@gmail.com>
2017-09-29 21:29:56 +0900

    use redux thunk2

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,26 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import { updateCurrent } from '../reducers/todo';
+import { updateCurrent, saveTodo } from '../reducers/todo';
 
 
-const TodoForm = (props) => {
-  const { currentTodo, updateCurrent } = props
-  const hangleInputChange = (evt) => {
+class TodoForm extends Component  {
+  hangleInputChange = (evt) => {
     const val = evt.target.value
-    updateCurrent(val)
+    this.props.updateCurrent(val)
   }
-  return (
-    <form>
-      <input
-        type="text"
-        onChange={hangleInputChange}
-        value={currentTodo}
-      />
-    </form>
-  )
+  
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+    this.props.saveTodo(this.props.currentTodo)
+  }
+  render() {
+    const { currentTodo } = this.props
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          onChange={this.hangleInputChange}
+          value={currentTodo}
+        />
+      </form>
+    )
+  }
 }
 
 export default connect(
   (state) => ({currentTodo: state.currentTodo}),
-  {updateCurrent}
+  {updateCurrent, saveTodo}
 )(TodoForm)
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,4 +1,4 @@
-import { getTodos } from '../lib/todoService';
+import { getTodos, createTodo } from '../lib/todoService';
 
 const initState = {
   todos: [],
@@ -11,16 +11,21 @@ const CURRENT_UPDATE = 'CURRENT_UPDATE'
 
 export const updateCurrent = (val) => ({type: CURRENT_UPDATE, payload: val})
 export const loadTodos = (todos) => ({type: TODOS_LOAD, payload: todos})
+export const addTodo = (todo) => ({type: TODO_ADD, payload: todo})
 export const fetchTodos = () => (dispatch) => {
   getTodos()
     .then(todos => dispatch(loadTodos(todos)))
 }
 
+export const saveTodo = (name) => (dispatch) => {
+  createTodo(name)
+    .then(res => dispatch(addTodo(res)))
+}
 
 export default (state = initState, action) => {
   switch (action.type) {
     case TODO_ADD:
-      return {...state, todos: [...state.todos, action.payload]}
+      return {...state, currentTodo: '', todos: [...state.todos, action.payload]}
     case TODOS_LOAD:
       return {...state, todos: action.payload}
     case CURRENT_UPDATE:
